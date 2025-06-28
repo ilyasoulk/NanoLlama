1f5e6ee86a624c200d7a7a3f60122c7c943daa1e
ilyas <oulkaddailyas@gmail.com>
2025-06-28 00:16:04 -0400

    rmsnorm

--- llama.py
+++ llama.py
@@ -56,12 +56,14 @@ class ROPE(nn.Module):
 
 
 class RMSNorm(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, d_model: int, eps: float = 1e-8) -> None:
         super().__init__()
-        pass
+        self.gain = nn.Parameter(torch.ones(d_model))
+        self.eps = eps
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x
+        rms = torch.mean(x**2, keepdim=True, dim=-1).sqrt()
+        return (x / rms + self.eps) * self.gain
 
 
 class SelfAttention(nn.Module):
